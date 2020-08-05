4a3420dc8c879e963df88e459ebb12b22fbc130a
vcaruso <vinnycar0923@gmail.com>
2020-08-05 16:06:40 -0700

    created new parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,14 @@
-CREATE TABLE candidates (
+CREATE TABLE candidates
+(
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
+);
+
+CREATE TABLE parties
+(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
 );
